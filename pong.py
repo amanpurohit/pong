f242ae317731594b1e65e37cf6f6528fdb3c2db1
amanpurohit <amanpurohit@users.noreply.github.com>
2015-07-17 23:27:09 +0530

    added functionality to keys

--- pong.py
+++ pong.py
@@ -3,6 +3,7 @@ Pong Game
 """
 
 import pygame
+from pygame.locals import K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -18,11 +19,22 @@ clock = pygame.time.Clock()
 circle_startx, circle_starty = 350, 250
 circle_speed = circle_dx, circle_dy = 5, 5
 rect_startx, rect_starty = 350, 480
+shift = 20
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_RIGHT:
+                rect_startx += shift
+            elif event.key == K_LEFT:
+                rect_startx += -shift
+        elif event.type == pygame.KEYUP:
+            if event.key == K_RIGHT:
+                rect_move = 0
+            elif event.key == K_LEFT:
+                rect_move == 0
 
     screen.fill(white)
 
@@ -34,10 +46,13 @@ while True:
     circle_startx += circle_dx
     circle_starty += circle_dy
 
+    # Collision with boundaries
     if circle_startx > 680 or circle_startx < 20:
         circle_dx = circle_dx * -1
-    if circle_starty > 480 or circle_starty < 20:
+    if circle_starty < 20:
         circle_dy = circle_dy * -1
+    if circle_starty > 480:
+        circle_startx, circle_starty = 350, 250
 
     clock.tick(20)  # limits frames per second
     pygame.display.flip()  # updates the screen
